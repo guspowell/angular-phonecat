844c0b0bfaf89fdb0432b143ff73370a6d0f1173
guspowell <augustuspowell@me.com>
2015-04-15 16:41:01 +0100

    altered to allow for minification

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,16 +2,15 @@
 
 /* Controllers */
 
+
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
 
   $http.get('phones/phones.json').success(function(data) {
-
     $scope.phones = data;
-
   });
 
   $scope.orderProp = 'age';
 
-});
+}]);
